27f8c4859a89a4323e5251e866fb9af928e3e096
Christian Gass <christiangass@outlook.com>
2018-03-04 16:29:26 -0500

    add trigger to address #5

--- db_setup.py
+++ db_setup.py
@@ -21,44 +21,48 @@ src_products = etl.fromcsv("sources/Products.csv")
 src_tags = etl.fromcsv("sources/Categories.csv")
 src_staff = etl.fromcsv("sources/Staff.csv")
 
+
 def create_suppliers():
     for row in etl.dicts(src_suppliers):
         new_supplier = Supplier(
-            id = row["ID"],
-            name = row["Company"]
+            id=row["ID"],
+            name=row["Company"]
         )
         db.session.add(new_supplier)
     db.session.commit()
 
+
 def create_products():
     for row in etl.dicts(src_products):
         new_product = Product(
-            code = row["ProductCode"],
-            name = row["ProductName"],
-            list_price = row["StandardCost"],
-            selling_price = row["ListPrice"],
-            quantity_per_unit = row["QuantityPerUnit"],
-            description = row["Description"],
-            supplier_id = row["SupplierID"],
-            discontinued = row["Discontinued"],
+            code=row["ProductCode"],
+            name=row["ProductName"],
+            list_price=row["StandardCost"],
+            selling_price=row["ListPrice"],
+            quantity_per_unit=row["QuantityPerUnit"],
+            description=row["Description"],
+            supplier_id=row["SupplierID"],
+            discontinued=row["Discontinued"],
         )
         db.session.add(new_product)
     db.session.commit()
 
+
 def create_tags():
     for row in etl.dicts(src_tags):
         new_row = Tag(
-            id = row["ID"],
-            name = row["Category"]
+            id=row["ID"],
+            name=row["Category"]
         )
         db.session.add(new_row)
     db.session.commit()
-        
+
+
 def create_staff():
     for row in etl.dicts(src_staff):
         new_row = Staff(
-            id = row["ID"],
-            name = row["FullName"]
+            id=row["ID"],
+            name=row["FullName"]
         )
         db.session.add(new_row)
     db.session.commit()
@@ -66,6 +70,7 @@ def create_staff():
 #----------------------------------------------------------------------------#
 # DB CONFIG
 #----------------------------------------------------------------------------#
+
 
 def set_trigger_fullname(db_path):
     q1 = """
@@ -104,11 +109,29 @@ def set_trigger_fullname(db_path):
         c.execute(each)
     conn.commit()
     conn.close()
-    
+
+
 def set_trigger_selling_price(db_path):
     q1 = """
     CREATE TRIGGER record_selling_price_at_time_of_sale
     AFTER INSERT ON sale
+    FOR EACH ROW
+    BEGIN
+        UPDATE sale
+        SET sold_price = (
+            CASE
+                WHEN (sale.special_price IS NOT NULL AND sale.special_price > 0)
+                    THEN sale.special_price
+                ELSE
+                    (SELECT product.selling_price FROM product WHERE product.id = sale.product_id)
+            END
+        )
+        WHERE id = new.id;
+    END;
+    """
+    q2 = """
+    CREATE TRIGGER update_selling_price_if_special_price_changes
+    AFTER UPDATE ON sale
     FOR EACH ROW
     BEGIN
         UPDATE sale
@@ -129,10 +152,11 @@ def set_trigger_selling_price(db_path):
         c.execute(each)
     conn.commit()
     conn.close()
-    
+
 #----------------------------------------------------------------------------#
 # CREATE SOME DATA
 #----------------------------------------------------------------------------#
+
 
 def build_db():
     # build tables from source data
@@ -144,8 +168,8 @@ def build_db():
     # set triggers
     set_trigger_fullname(db_path)
     set_trigger_selling_price(db_path)
-        
-        
+
+
 if __name__ == '__main__':
     db.drop_all()
     db.create_all()
